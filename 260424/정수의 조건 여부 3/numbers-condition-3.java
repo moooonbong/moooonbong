cbe1e0208b35ef56c939b62a513b4f48e8ac244d
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-04-24 22:04:11 +0900

    Title: 정수의 조건 여부 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260424/정수의 조건 여부 3/numbers-condition-3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        if (input % 13 == 0 || input % 19 ==0)
+            System.out.println(true);
+        else 
+            System.out.println(false);
+    }
+}
