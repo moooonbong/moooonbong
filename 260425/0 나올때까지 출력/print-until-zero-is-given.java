d0ea13f481b8251100f47b4b42a0b0a1ca3e8f8e
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-04-25 16:46:26 +0900

    Title: 0 나올때까지 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260425/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int input = sc.nextInt();
+
+            if (input == 0){
+                break;
+            }
+
+            System.out.println(input);
+        }
+    }
+}
