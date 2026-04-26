a3d1f135e1d049f2ea323cda046a40a002b89041
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-04-26 22:24:41 +0900

    Title: 두 배열의 곱, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260426/두 배열의 곱/multiple-of-two-arrays.java
+++ 260426/두 배열의 곱/multiple-of-two-arrays.java
@@ -12,7 +12,13 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int y=0; y<3; y++){
                 arr[x][y] = Integer.parseInt(st.nextToken());
-                brr[x][y] = arr[x][y] + 1;
+            }
+        }
+         br.readLine();
+        for (int x = 0; x<3; x++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int y=0; y<3; y++){
+                brr[x][y] = Integer.parseInt(st.nextToken());
                 System.out.print((arr[x][y] * brr[x][y]) + " ");
             }
             System.out.println();
