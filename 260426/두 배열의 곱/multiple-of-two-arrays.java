01031f9fe31a83f14b145c58a1ca988177899f65
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-04-26 22:18:16 +0900

    Title: 두 배열의 곱, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260426/두 배열의 곱/multiple-of-two-arrays.java
+++ 260426/두 배열의 곱/multiple-of-two-arrays.java
@@ -6,12 +6,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[][] arr = new int[3][3];
+        int[][] brr = new int[3][3];
 
         for (int x=0; x<3; x++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int y=0; y<3; y++){
-                arr[x][y] = Integer.parseInt(st.nextToken()) + 1;
-                System.out.print(arr[x][y] + " ");
+                arr[x][y] = Integer.parseInt(st.nextToken());
+                brr[x][y] = arr[x][y] + 1;
+                System.out.print((arr[x][y] * brr[x][y]) + " ");
             }
             System.out.println();
         }
