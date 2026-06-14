335d6ee56ad7ad6a1ee4378f39aae77f3e02dfa9
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-14 21:22:00 +0900

    [Passed] 합과 평균의 차 (95ms, 9MB)

---
+++ 260614/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int sum = a+b+c;
+        int avg = sum /3;
+        int minus = sum - avg;
+
+        System.out.println(sum + "\n" + avg +"\n" + minus);
+    }
+}
