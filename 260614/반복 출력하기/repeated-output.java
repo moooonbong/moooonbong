8d5b7806f6346e10f801ba4b74266c4ce7cbeeef
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-14 18:04:56 +0900

    [Passed] 반복 출력하기 (128ms, 10MB)

---
+++ 260614/반복 출력하기/repeated-output.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for (int x=0; x<a; x++){
+            System.out.println("12345^&*()_");
+        }
+    }
+}
