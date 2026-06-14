11ae442cbd30a139d16e27810930c69f051fd160
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-14 17:07:33 +0900

    [Passed] 문자 변경하기 (93ms, 9MB)

---
+++ 260614/문자 변경하기/change-charater.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        char a = 'C';
+        a = 'T';
+        System.out.println(a);    }
+}
