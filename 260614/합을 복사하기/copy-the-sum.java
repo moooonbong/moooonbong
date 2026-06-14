1f3ec78057b468d567e236d421cf729e41de9220
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-14 17:39:30 +0900

    [Passed] 합을 복사하기 (95ms, 9MB)

---
+++ 260614/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int sum = a + b + c;
+
+        a = sum;
+        b = sum;
+        c = sum;
+
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
