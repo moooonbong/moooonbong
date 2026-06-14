256d3855f12697492012e068f85d2c197896d838
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-14 18:14:27 +0900

    [Passed] 최대공약수 구하기 (125ms, 10MB)

---
+++ 260614/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    static int gcp (int a, int b){
+        if (b == 0){
+            return a;
+        }
+
+        return gcp(b, a%b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(gcp (m, n));
+    }
+}
