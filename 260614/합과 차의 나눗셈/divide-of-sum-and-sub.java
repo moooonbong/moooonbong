074c0b674861cfbdbe38c4f25211524ebe8bce5a
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-14 21:17:42 +0900

    [Passed] 합과 차의 나눗셈 (96ms, 9MB)

---
+++ 260614/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        double sum = a + b;
+
+        double minus = a - b;
+
+        System.out.printf("%.2f", (double)(sum/minus));
+    }
+}
