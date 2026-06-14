12d2319455b4dc9e4b18e0ae2824e50a984d9d9b
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-14 17:45:08 +0900

    [Passed] 길이 단위 환산하기 (156ms, 12MB)

---
+++ 260614/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        double N = sc.nextDouble();
+
+        System.out.printf("%.1f", N * 30.48);
+    }
+}
