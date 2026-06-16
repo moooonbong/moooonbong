8f0297369b6a7c4defbeb7060092085491989cf3
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-16 20:41:14 +0900

    [Passed] 두 수의 짝홀 여부 (91ms, 9MB)

---
+++ 260616/두 수의 짝홀 여부/parity-of-two-numbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a % 2 == 0)
+            System.out.println("even");
+        else 
+            System.out.println("odd");
+
+        if (b % 2 == 0)
+            System.out.println("even");
+        else 
+            System.out.println("odd");
+    }
+}
