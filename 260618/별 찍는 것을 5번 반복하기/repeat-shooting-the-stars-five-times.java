5a70fc73bdfe6cc20ecf6503f954fe7f7c5e73f7
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-18 18:47:55 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (95ms, 9MB)

---
+++ 260618/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,13 @@
+public class Main {
+    static void func(){
+        for (int x=0; x<10; x++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // Please write your code here.
+        for (int x=0; x<5; x++)
+            func();
+    }
+}
