1b39efa4cf107811981d657b9791124625872c1f
ChulMin <133864111+moooonbong@users.noreply.github.com>
2026-06-18 18:18:20 +0900

    [Passed] 별 N개 출력하기 (135ms, 10MB)

---
+++ 260618/별 N개 출력하기/print-n-stars.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int value = 1;
+
+        while(value<=a){
+            value++;
+            System.out.println("*");
+        }
+    }
+}
